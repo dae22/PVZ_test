7467c41a677128e93e5de52dd03878e83df8abdf
dae22 <dae-22@mail.ru>
2025-04-21 11:33:52 +0300

    change datatype in tables

--- app/init_db.py
+++ app/init_db.py
@@ -17,7 +17,7 @@ async def create_table_users():
 async def create_table_pickup():
     conn = await asyncpg.connect(DATABASE_URL)
     await conn.execute(''' CREATE TABLE IF NOT EXISTS pickup_points (
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     registration_date TIMESTAMP NOT NULL,
     city TEXT NOT NULL
     )
@@ -27,7 +27,7 @@ async def create_table_pickup():
 async def create_table_receptions():
     conn = await asyncpg.connect(DATABASE_URL)
     await conn.execute('''CREATE TABLE IF NOT EXISTS receptions (
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     date_time TIMESTAMP NOT NULL,
     pickup_point_id UUID REFERENCES pickup_points(id),
     status TEXT NOT NULL
@@ -38,7 +38,7 @@ async def create_table_receptions():
 async def create_table_products():
     conn = await asyncpg.connect(DATABASE_URL)
     await conn.execute('''CREATE TABLE IF NOT EXISTS products (
-    id UUID PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     date_time TIMESTAMP NOT NULL,
     reception_id UUID REFERENCES receptions(id),
     product_type TEXT NOT NULL
